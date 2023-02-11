bc41522fed21b528ce270436d3eece751fb26dd7
Alex Barongo <alexbarongo1@gmail.com>
2023-02-11 11:03:01 +0300

    removed the for loop

--- tempConversion.c
+++ tempConversion.c
@@ -33,9 +33,6 @@ int main()
     printf("%3.0f\t%6.1f\n", celsius, fahr);
     celsius = celsius + step;
   }
-
-/*Using for loop*/
-
-  for(fahr = lower; fahr <= upper; fahr + 20)
-    printf("%3d\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
+
+
